d66fcdff28bfac6a97b624748e8084178012d5dd
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-02-18 18:03:59 -0500

    added livestream search feature

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from googleapiclient.discovery import build
 
+# TODO: REPLACE THIS API KEY BEFORE ADDING TO GITHUB!!!
 api_key = 'AIzaSyBk1k4xQM8NCr-OfUBTVFO_lKQilf6WMHU'
 
+# Create a service from our google api with the service name, version and out api key
 service = build('youtube', 'v3', developerKey=api_key)
 
-request = service.channels().list(
-            part='statistics',
-            forUsername='schafer5'
-        )
+# Create a request to get live stream info from a channel
+request = service.search().list(
+    part='snippet',
+    channelId='UCK9V2B22uJYu3N7eR_BT9QA',
+    type='video',
+    eventType='live'
+)
 
+# Execute that request
 response = request.execute()
 
+# Print the result. It'll be formatted badly but whatever
 print(response)
-
