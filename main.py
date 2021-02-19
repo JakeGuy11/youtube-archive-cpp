1b43b2ed4095ac74c05b2f0b626f9a1bb3150bd3
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-02-18 20:56:11 -0500

    added channel search functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from googleapiclient.discovery import build
 
 # TODO: REPLACE THIS API KEY BEFORE ADDING TO GITHUB!!!
@@ -7,18 +8,33 @@ api_key = 'AIzaSyBk1k4xQM8NCr-OfUBTVFO_lKQilf6WMHU'
 # Create a service from our google api with the service name, version and out api key
 service = build('youtube', 'v3', developerKey=api_key)
 
+# -----Find the channel's id from their name-----
+
 # Create a request to get live stream info from a channel
-request = service.search().list(
+channelRequest = service.channels().list(
     part='snippet',
-    channelId='UCK9V2B22uJYu3N7eR_BT9QA',
+    id=sys.argv[1]
+)
+# Execute that request
+channelResponse = channelRequest.execute()
+
+print(channelResponse["items"][0]["snippet"]["title"])
+
+# -----Get any running livestreams-----
+
+# Create a request to get live stream info from a channel
+liveRequest = service.search().list(
+    part='snippet',
+    channelId=sys.argv[1],
     type='video',
     eventType='live'
 )
-
 # Execute that request
-response = request.execute()
+liveResponse = liveRequest.execute()
 
-info = json.dumps(response, indent=4)
-
-# Print the result. It'll be formatted badly but whatever
-print(response["items"][0]["id"]["videoId"])
+try:
+    streamId = liveResponse["items"][0]["id"]["videoId"]
+    # Print the resulting video id. In the future, this'll be used in the youtube-dl command to capture the livestream
+    print(streamId)
+except:
+    print("Stream does not exist or has not started")
