e463173a552628341bc6736ac16cdf53885a26bf
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-02-19 12:19:36 -0500

    added help page

--- run.cpp
+++ run.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+#include<fstream>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "hello world" << std::endl;
+
+    if(argc == 1)
+    {
+    std::ifstream helpFile("help");
+    if (helpFile.is_open()) std::cout << helpFile.rdbuf();
+    }
+
 }
