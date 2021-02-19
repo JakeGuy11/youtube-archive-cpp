ddc30657989ade200d603e3dc67c08a54e86aa4c
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-02-18 22:46:50 -0500

    commented entire program

--- dl-script.bash
+++ dl-script.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "Downloading stream $1 into $2"
-echo "Line 2 here"
 
-ffmpeg -i `youtube-dl -f best -g $1` $2
+# Create a var to hold the full file path
+filePath="$2"
+
+# Create a file to let the main program know we're already downloading this youtuber's stream
+echo "Creating ${filePath%/*}/.$3"
+touch "${filePath%/*}/.$3"
+
+# Download the actual stream
+ffmpeg -i $(youtube-dl -f best -g "$1") "$2"
+
+# Delete that file to let the main program know we're no longer recording the stream
+echo "Deleting ${filePath%/*}/.$3"
+rm "${filePath%/*}/.$3"
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/bash
+
+# Grab all needed imports
 import sys
 import subprocess
 from pathlib import Path
@@ -10,16 +13,20 @@ api_key = 'AIzaSyBk1k4xQM8NCr-OfUBTVFO_lKQilf6WMHU'
 # Create a service from our google api with the service name, version and out api key
 service = build('youtube', 'v3', developerKey=api_key)
 
-# -----Find the channel's id from their name-----
+# -----Find the channel info from their id-----
 
 # Create a request to get live stream info from a channel
 channelRequest = service.channels().list(
+    # Get the a bunch of information (called a snippet)
     part='snippet',
+    # Get it for the channel with the specified id
     id=sys.argv[1]
 )
+
 # Execute that request
 channelResponse = channelRequest.execute()
 
+# For now all we need is the channel name, although we're not using it right now
 channelName = channelResponse["items"][0]["snippet"]["title"]
 print(channelName)
 
@@ -27,31 +34,46 @@ print(channelName)
 
 # Create a request to get live stream info from a channel
 liveRequest = service.search().list(
+    # Get a snippet with the info needed
     part='snippet',
+    # Set the channel id to the one put through the command line
     channelId=sys.argv[1],
+    # Look for a video
     type='video',
+    # Make sure that video is a livestream
     eventType='live'
 )
+
 # Execute that request
 liveResponse = liveRequest.execute()
 
 # -----Set the file name-----
 
+# Get the home path
 homePath = str(Path.home())
+# Get the current date and time, DD-MM-YYYY
 currentDate = date.today().strftime("%d-%m-%Y-")
-streamPath = homePath + currentDate + channelName.replace(" ", "-") + ".ts"
+# Generate the path to save the stream
+streamPath = homePath + "/.holo-dl/" + currentDate + sys.argv[2] + ".ts"
+# Print the stream path
 print(streamPath)
 
 # -----Start the script that will download the stream-----
 
 try:
+    # Get the video id (the part after watch?v=)
     streamId = liveResponse["items"][0]["id"]["videoId"]
-    # Print the resulting video id. In the future, this'll be used in the youtube-dl command to capture the livestream
+    # Print the resulting video id. In the future
     print(streamId)
+    # Generate the full video URL
     youtubeURL = "https://www.youtube.com/watch?v=" + streamId
+    # Print that URL
     print(youtubeURL)
-    proc = subprocess.check_output(["./dl-script.bash", youtubeURL, "/home/jake/Documents/strm.ts"])
+    # Create and start a process to download the stream
+    proc = subprocess.check_output(["./dl-script.bash", youtubeURL, streamPath, sys.argv[2]])
+    # Print that process output
     for outLine in proc.splitlines():
         print(outLine)
 except:
+    # Something went wrong, most likely that the user is not streaming
     print("Stream does not exist or has not started")
