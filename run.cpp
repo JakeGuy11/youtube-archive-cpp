8434f09fa3a7d10a2027bbdc3b6ce518cedd138d
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-02-20 15:56:43 -0500

    added partial argument parsing

--- run.cpp
+++ run.cpp
@@ -1,5 +1,24 @@
 #include<iostream>
 #include<fstream>
+#include<list>
+#include<string>
+#include<array>
+#include<vector>
+
+std::vector<std::pair<std::string,std::string>> sessionPref(0);
+std::vector<std::pair<std::string,std::string>> updatedPref;
+std::string prefString = "";
+std::string homeDir = getenv("HOME");
+std::string holoDir = homeDir + "/.holo-dl/";
+
+std::string parseChannelURL(std::string url)
+{
+    std::size_t occurrence = url.find_last_of("/");
+    std::string channelWithSlash = url.substr(occurrence);
+
+    channelWithSlash.erase(0,1);
+    return channelWithSlash;
+}
 
 void help()
 {
@@ -9,14 +28,27 @@ void help()
 
 void add(std::string channelId, std::string nickname)
 {
-    std::cout << "Channel URL: " << channelId << std::endl
-              << "Nickname: " << nickname << std::endl;
+    std::pair<std::string,std::string> tempPair (channelId,nickname);
+    sessionPref.push_back(tempPair);
+    for(std::pair<std::string,std::string> currentPair : sessionPref)
+    {
+        std::cout << currentPair.first << ", " << currentPair.second << std::endl;
+    }
 }
 
-void remove(std::string channelId, std::string nickname)
+void pullList()
 {
-    std::cout << "Channel URL: " << channelId << std::endl
-              << "Nickname: " << nickname << std::endl;
+
+}
+
+void pushList()
+{
+
+}
+
+void remove(std::string remArg)
+{
+
 }
 
 void list()
@@ -42,13 +74,15 @@ void includeTemp(std::string channelId)
 int main(int argc, char **argv)
 {
 
+    std::cout << holoDir << std::endl;
+
     if(argc < 2)
     {
     std::cerr << "Usage: " << std::endl
         << argv[0] << " --start" << std::endl
         << argv[0] << " --help" << std::endl
         << argv[0] << " --list" << std::endl;
-    return 1;
+    //return 1;
     }
 
     for(int i = 1; i < argc; ++i)
@@ -56,13 +90,18 @@ int main(int argc, char **argv)
         if (std::string(argv[i]) == "-h" || std::string(argv[i]) == "--help") {
             help();
         }else if (std::string(argv[i]) == "-a" || std::string(argv[i]) == "--add") {
-            std::string channelId = argv[i+1];
-            std::string nickname = argv[i+2];
-            add(channelId, nickname);
+            try {
+                std::string channelId = parseChannelURL(argv[i+1]);
+                std::string nickname = argv[i+2];
+                add(channelId, nickname);
+            }
+            catch (...)
+            {
+            std::cout << "The channel URL you've entered is not valid. Please retry." << std::endl;
+            }
         }else if (std::string(argv[i]) == "-r" || std::string(argv[i]) == "--list") {
-            std::string channelId = argv[i+1];
-            std::string nickname = argv[i+2];
-            remove(channelId, nickname);
+            std::string remArg = argv[i+1];
+            remove(remArg);
         }else if (std::string(argv[i]) == "-l" || std::string(argv[i]) == "--list") {
             list();
         }else if (std::string(argv[i]) == "-s" || std::string(argv[i]) == "--start") {
