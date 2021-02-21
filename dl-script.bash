0c6b023106ba4c49aa08bf2136c68a2df2141e04
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-02-21 08:56:10 -0500

    added preference saving and parsing

--- dl-script.bash
+++ dl-script.bash
@@ -8,7 +8,7 @@ echo "Creating ${filePath%/*}/.$3"
 touch "${filePath%/*}/.$3"
 
 # Download the actual stream
-ffmpeg -i $(youtube-dl -f best -g "$1") "$2"
+ffmpeg -i "$(youtube-dl -f best -g "$1")" "$2"
 
 # Delete that file to let the main program know we're no longer recording the stream
 echo "Deleting ${filePath%/*}/.$3"
--- run.cpp
+++ run.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
 #include<fstream>
-#include<list>
-#include<string>
-#include<array>
 #include<vector>
+#include<sstream>
 
 std::vector<std::pair<std::string,std::string>> sessionPref;
 std::vector<std::pair<std::string,std::string>> otPref;
-std::vector<std::pair<std::string,std::string>> updatedPref;
 std::string prefString = "";
 std::string homeDir = getenv("HOME");
 std::string holoDir = homeDir + "/.holo-dl/";
+std::string holoPref = homeDir + "/.holo-dl/.queue";
 int timeInterval = 120;
 
 std::string parseChannelURL(std::string url)
@@ -20,6 +18,63 @@ std::string parseChannelURL(std::string url)
 
     channelWithSlash.erase(0,1);
     return channelWithSlash;
+}
+
+std::vector<std::pair<std::string,std::string>> splitIntoVector(std::string const &str)
+{
+    std::vector<std::pair<std::string,std::string>> returnVector;
+
+    std::stringstream tempStream(str);
+    std::string foundString;
+
+    std::vector<std::string> tempVec;
+    std::pair<std::string,std::string> tempPair;
+
+    while (std::getline(tempStream, foundString, '|')) {
+        tempVec.push_back(foundString);
+    }
+
+    bool added = false;
+
+    for(int i = 0; i < tempVec.size(); i++)
+    {
+        std::stringstream tempStream(tempVec[i]);
+        while (std::getline(tempStream, foundString, ';')) {
+            if(added)
+            {
+                tempPair.second = foundString;
+                added = false;
+            }
+            else
+            {
+                tempPair.first = foundString;
+                added = true;
+            }
+        }
+        returnVector.push_back(tempPair);
+    }
+
+    return returnVector;
+
+}
+
+std::string saveToString(std::vector<std::pair<std::string,std::string>> vectorToConvert)
+{
+
+    std::string returnString;
+
+    for(int i = 0; i < vectorToConvert.size(); i++)
+    {
+        if(i == 0)
+        {
+            returnString = vectorToConvert[i].first + ";" + vectorToConvert[i].second;
+        }
+        else
+        {
+            returnString += "|" + vectorToConvert[i].first + ";" + vectorToConvert[i].second;
+        }
+    }
+    return returnString;
 }
 
 void help()
@@ -125,6 +180,10 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    sessionPref = splitIntoVector("preUrl1;preName1|preUrl2;preName2");
+
+    bool run = false;
+
     for(int i = 1; i < argc; ++i)
     {
         if (std::string(argv[i]) == "-h" || std::string(argv[i]) == "--help") {
@@ -145,7 +204,7 @@ int main(int argc, char **argv)
         }else if (std::string(argv[i]) == "-l" || std::string(argv[i]) == "--list") {
             list();
         }else if (std::string(argv[i]) == "-s" || std::string(argv[i]) == "--start") {
-            start();
+            run = true;
         }else if (std::string(argv[i]) == "-i" || std::string(argv[i]) == "--interval") {
             std::string tempInterval = argv[i+1];
             setInterval(std::stoi(tempInterval));
@@ -162,5 +221,7 @@ int main(int argc, char **argv)
         }
 
     }
+
+    std::cout << saveToString(sessionPref) << std::endl;
 
 }
